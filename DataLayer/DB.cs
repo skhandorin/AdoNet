9f4d67d1d77a4a7085679e5a66fa6b54e6dd023c
SKhandorin <hsatig@mail.ru>
2018-03-28 23:46:23 +0500

    Connect to a Data Source

--- DataLayer/DB.cs
+++ DataLayer/DB.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,43 @@ namespace DataLayer
 {
     public class DB
     {
+        public static string ConnectionString
+        {
+            get
+            {
+                string connString = ConfigurationManager.ConnectionStrings["AWConnection"].ToString();
+
+                SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder(connString);
+                stringBuilder.ApplicationName = ApplicationName ?? stringBuilder.ApplicationName;
+                stringBuilder.ConnectTimeout = (ConnectionTimeout > 0) ? ConnectionTimeout : stringBuilder.ConnectTimeout;
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Returns an opened connection to the caller
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection GetSqlConnection()
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+
+        /// <summary>
+        /// Overrides the connection timeout
+        /// </summary>
+        public static int ConnectionTimeout { get; set; }
+
+        /// <summary>
+        /// Property used to override the name of the application
+        /// </summary>
+        public static string ApplicationName
+        {
+            get;
+            set;
+        }
     }
 }
--- WinDemo/Form1.cs
+++ WinDemo/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,14 @@ namespace WinDemo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connString = DataLayer.DB.ConnectionString;
+            DataLayer.DB.ApplicationName = "WinDemo Application";
+            DataLayer.DB.ConnectionTimeout = 30;
+            SqlConnection conn = DataLayer.DB.GetSqlConnection();
         }
     }
 }
